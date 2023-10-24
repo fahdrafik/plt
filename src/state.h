9a483c16aef4162360737e8d39dbc8652627dba8
zeicksoule <102954378+zeicksoule@users.noreply.github.com>
2023-10-24 14:39:37 +0200

    Add files via upload

--- src/state.h
+++ src/state.h
@@ -1,167 +1,178 @@
+//
+// Created by ZAIK on 24/10/2023.
+//
+
+#ifndef PLT_STATE_H
+#define PLT_STATE_H
+#endif //PLT_STATE_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
 
 namespace state {
 
-  /// class Decks - 
-  class Decks {
-    // Attributes
-  private:
-    int id;
-    Cards[] cardsInDeck;
-    Cards[] cardsInHand;
-    int numberOfCards;
-    // Operations
-  public:
-    void shuffle ();
-    void drawCard ();
-    void addCard ();
-    void removeCard ();
-  };
+    enum trapEffect {
+        NormalTrap,
+        ContinuousTrap ,
+        Counter
+    };
 
-  enum GameStatus {
-    PLAYING,
-    WON,
-    LOOSE
-  };
+    enum spellEffect {
+        Ritual,
+        NormalSpell,
+        ContinuousSpell,
+        Field ,
+        Equipped,
+        Quickplay
+    };
 
-  /// class Boards - 
-  class Boards {
-    // Attributes
-  private:
-    int id;
-    Monsters[] monsters;
-    Spells[] spells;
-    Traps[] traps;
-    Cards[] graveyard;
-    // Operations
-  public:
-    void attackPosition ();
-    void defensePosition ();
-    void addMonster ();
-    void removeMonster ();
-    void addSpell ();
-    void removeSpell ();
-    void addTrap ();
-    void removeTrap ();
-    void appel ();
-  };
+    /// class CardTypes -
+    class CardTypes {
+        // Attributes
+    private:
+        string type;
+        // Operations
+    public:
+        void isMonster ();
+        void isSpell ();
+        void isTrap ();
+    };
 
-  /// class Players - 
-  class Players {
-    // Attributes
-  private:
-    int id;
-    GameStatus status;
-    Decks deck;
-    Boards board;
-    int lifepoints;
-    // Operations
-  public:
-    void drawCard ();
-    void placeCard ();
-    void setSpell ();
-    void endTurn ();
-    void looseLifePoints (int degat );
-  };
+    /// class Cards -
+    class Cards {
+        // Attributes
+    protected:
+        int idCard;
+        string name;
+        string description;
+        CardTypes type;
+    };
 
-  /// class CardTypes - 
-  class CardTypes {
-    // Attributes
-  private:
-    string type;
-    // Operations
-  public:
-    void isMonster ();
-    void isSpell ();
-    void isTrap ();
-  };
+    /// class Spells -
+    class Spells {
+        // Attributes
+    private:
+        string effect;
+        // Operations
+    public:
+        bool isOn ();
+    };
 
-  /// class Cards - 
-  class Cards {
-    // Attributes
-  protected:
-    int idCard;
-    string name;
-    string description;
-    CardTypes type;
-    etat  state ;
-  };
+    /// class Traps -
+    class Traps {
+        // Attributes
+    private:
+        trapEffect effect;
+        // Operations
+    public:
+        void activate ();
+        bool isOn ();
+    };
 
-  /// class Calculation - 
-  class Calculation {
-    // Attributes
-  private:
-    int  lifePointsPlayer1;
-    int lifePointsPlayer2;
-    int attackPointsPlayer1;
-    int attackPointsPlayer2;
-    int defensePointsPlayer1;
-    int defensePointsPlayer2;
-    // Operations
-  public:
-    int getDefensePointsPlayer1 ();
-    int getDefensePointsPlayer2 ();
-    int getAttackPointsPlayer1 ();
-    int getAttackPointsPlayer2 ();
-    int getLifePointsPlayer1 ();
-    int getLifePointsPlayer2 ();
-  };
+    /// class Monsters -
+    class Monsters {
+        // Attributes
+    public:
+        int effet ;
+    private:
+        int level;
+        int attack;
+        int defense;
+        bool position;
+        // Operations
+    public:
+        void attackCard (Cards c);
+        void setSpell ();
+    };
 
-  enum TypePlayer {
-    HUMAN,
-    BOT
-  };
+    /// class Decks -
+    class Decks {
+        // Attributes
+    private:
+        int id;
+        int numberOfCards;
+        vector<Cards> cardsInDeck;
+        vector<Cards> cardsInHand;
+        // Operations
+    public:
+        ~Decks();
+        void shuffle ();
+        void drawCard ();
+        void addCard ();
+        void removeCard ();
+    };
 
-  /// class Spells - 
-  class Spells {
-    // Attributes
-  private:
-    string effect;
-    // Operations
-  public:
-    bool isOn ();
-  };
+    enum GameStatus {
+        PLAYING,
+        WON,
+        LOOSE
+    };
 
-  /// class Monsters - 
-  class Monsters {
-    // Attributes
-  public:
-    int effet ;
-  private:
-    int level;
-    int attack;
-    int defense;
-    bool position;
-    // Operations
-  public:
-    void attack (carte c);
-    void setSpell ();
-  };
+    /// class Boards -
+    class Boards {
+        // Attributes
+    private:
+        int id;
+        vector<Monsters>  monsters;
+        vector<Spells>  spells;
+        vector<Traps>  traps;
+        vector<Cards>  graveyard;
+        // Operations
+    public:
+        void attackPosition ();
+        void defensePosition ();
+        void addMonster ();
+        void removeMonster ();
+        void addSpell ();
+        void removeSpell ();
+        void addTrap ();
+        void removeTrap ();
+        void appel ();
+    };
 
-  enum trapEffect {
-    Normal,
-    Continuous ,
-    Counter
-  };
+    /// class Players -
+    class Players {
+        // Attributes
+    private:
+        int id;
+        GameStatus status;
+        Decks deck;
+        Boards board;
+        int lifepoints;
+        // Operations
+    public:
+        void drawCard ();
+        void placeCard ();
+        void setSpell ();
+        void endTurn ();
+        void looseLifePoints (int degat );
+    };
 
-  /// class Traps - 
-  class Traps {
-    // Attributes
-  private:
-    trapEffect effect;
-    // Operations
-  public:
-    void activate ();
-    bool isOn ();
-  };
+    /// class Calculation -
+    class Calculation {
+        // Attributes
+    private:
+        int  lifePointsPlayer1;
+        int lifePointsPlayer2;
+        int attackPointsPlayer1;
+        int attackPointsPlayer2;
+        int defensePointsPlayer1;
+        int defensePointsPlayer2;
+        // Operations
+    public:
+        int getDefensePointsPlayer1 ();
+        int getDefensePointsPlayer2 ();
+        int getAttackPointsPlayer1 ();
+        int getAttackPointsPlayer2 ();
+        int getLifePointsPlayer1 ();
+        int getLifePointsPlayer2 ();
+    };
 
-  enum spellEffect {
-    Ritual,
-    Normal,
-    Continuous,
-    Field ,
-    Equipped,
-    Quickplay
-  };
-
+    enum TypePlayer {
+        HUMAN,
+        BOT
+    };
 };
 
