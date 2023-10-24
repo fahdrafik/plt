33022be69af60dd52bbd4072e2da2afb313d8547
CornicRomain <superrom1.cornic@gmail.com>
2023-10-24 14:31:14 +0200

    .cpp v1

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -23,3 +23,26 @@ int main(int argc,char* argv[])
 
     return 0;
 }
+/*
+ *     // Créez des joueurs
+    Player player1("Joueur 1");
+    Player player2("Joueur 2");
+
+    // Créez un état de jeu initial avec les joueurs
+    State gameState({player1, player2});
+
+    // Démarrez le jeu
+    gameState.startGame();
+
+    // Exécutez un certain nombre de tours ou un scénario de jeu ici
+    // gameState.playTurn();
+    // ...
+
+    // Terminez le jeu
+    gameState.endGame();
+
+    // Affichez le score ou l'état final du jeu
+    gameState.displayScore();
+
+    return 0;
+ */
