be547b589163553942c2e651963a9926e1ca1b7f
zeicksoule <102954378+zeicksoule@users.noreply.github.com>
2023-11-07 14:10:31 +0100

    Add files via upload

---
+++ src/shared/state/CardTypes.cpp
@@ -0,0 +1,23 @@
+#include "CardTypes.h"
+#include "state.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+using namespace state; 
+
+CardTypes::CardTypes(std::string type) {
+    this->type = type;
+}
+void CardTypes::isMonster() {
+    cout << "isMonster\n";
+}
+void CardTypes::isSpell() {
+    cout << "isSpell\n";
+}
+void CardTypes::isTrap() {
+    cout << "isTrap\n";
+}
---
+++ src/shared/state/Cards.cpp
@@ -0,0 +1,12 @@
+#include "Cards.h"
+#include "state.h"
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace state; 
+
+Cards::Cards(int idCard, std::string name, std::string description, CardTypes typeCarte, int nombresDeCartes):typeCarte(typeCarte) {
+
+}
