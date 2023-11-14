155024e370794d8f5deb73f4f929085b25f4f2af
zeicksoule <zeicksoule@gmail.com>
2023-11-14 11:24:02 +0100

    Ajout test Calculation.cpp

--- test/shared/test_shared_dummy.cpp
+++ test/shared/test_shared_dummy.cpp
@@ -14,25 +14,24 @@ BOOST_AUTO_TEST_CASE(TestStaticAssert)
 
 BOOST_AUTO_TEST_CASE(TestCarte)
 {
-  {
       CardTypes CarteMonstre ("Monstres");
       Cards DragonBlancAuxYeuxBleus (0,"Dragon Blanc Aux Yeux Bleus","Monstre de type dragon",CarteMonstre);
       BOOST_CHECK_EQUAL(DragonBlancAuxYeuxBleus.getIdCard(), 0);
       DragonBlancAuxYeuxBleus.setIdCard(1);
       BOOST_CHECK_EQUAL(DragonBlancAuxYeuxBleus.getIdCard(), 1);
-  }
-
-  {
-      // Exemple ex;
-      //ex.setX(21);
-      //BOOST_CHECK_LE(ex.x, 32); // Less than equal
-      //BOOST_CHECK_GT(ex.x, 11); // Greater than equl
-  }
 }
 
 BOOST_AUTO_TEST_CASE(TestCalculation)
 {
     Calculation Calcul(2000,1000,4000,5000,2000,4000);
+    BOOST_CHECK_EQUAL(Calcul.getLifePointsPlayer1(),2000);
+    Calcul.setAttackPointsPlayer1(1500);
+    BOOST_CHECK_EQUAL(Calcul.getAttackPointsPlayer1(),1500);
+}
+
+BOOST_AUTO_TEST_CASE(TestBoards)
+{
+    Boards board
 }
 
 /* vim: set sw=2 sts=2 et : */
