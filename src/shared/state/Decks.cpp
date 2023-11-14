79dd10ff97a49971f76164c272680542605e66a3
fahdrafik <102949405+fahdrafik@users.noreply.github.com>
2023-11-14 20:00:44 +0100

    Add files via upload

--- src/shared/state/Decks.cpp
+++ src/shared/state/Decks.cpp
@@ -1,26 +1,28 @@
 //
-// Created by cornic on 14/11/23.
+// Created by elmoro on 14/11/23.
 //
-#include "Decks.h"
-
-
-
+#include "Players.h"
+#include "States.h"
 #include <iostream>
 #include <vector>
 
-using namespace std;
 using namespace state;
+using namespace std ;
 
-Decks::Decks (std::vector<Cards>
-              cardsInDeck,
-              std::vector <Cards> cardsInHands
-){}
+Decks::Decks (std::vector<Cards> cardsInDeck, std::vector<Cards> cardsInHand) {
+    this->cardsInDeck = cardsInDeck;
+    this->cardsInHand = cardsInHand;
+}
 
-
-void Decks::shuffle(){}
-
-void Decks::drawCard(){}
-
-void Decks::addCard(){}
-
-void Decks::removeCard(){}
+void shuffle () {
+    cout << "shuffle is done";
+}
+void drawCard (){
+    cout <<"Cards Drawn";
+}
+void addCard () {
+    cout << "Card added ";
+}
+void removeCard () {
+    cout << " Card removed to Player";
+}
