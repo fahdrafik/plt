42dc4be0579a9b86be1baae2e05238ab32d9d713
elmoro <fahd.rafik@ensea.fr>
2023-11-22 17:11:04 +0100

    fahed

--- test/shared/test_shared_dummy.cpp
+++ test/shared/test_shared_dummy.cpp
@@ -4,21 +4,25 @@
 #include "../../src/shared/state/Cards.h"
 #include "../../src/shared/state/CardTypes.h"
 #include "../../src/shared/state/Calculation.h"
+//#include "../../src/shared/state/Boards.h"
+#include "../../src/shared/state/Spells.h"
+#include "../../src/shared/state/Traps.h"
+
 
 using namespace ::state;
 
 BOOST_AUTO_TEST_CASE(TestStaticAssert)
 {
-  BOOST_CHECK(1);
+    BOOST_CHECK(1);
 }
 
 BOOST_AUTO_TEST_CASE(TestCarte)
 {
-      CardTypes CarteMonstre ("Monstres");
-      Cards DragonBlancAuxYeuxBleus (0,"Dragon Blanc Aux Yeux Bleus","Monstre de type dragon",CarteMonstre);
-      BOOST_CHECK_EQUAL(DragonBlancAuxYeuxBleus.getIdCard(), 0);
-      DragonBlancAuxYeuxBleus.setIdCard(1);
-      BOOST_CHECK_EQUAL(DragonBlancAuxYeuxBleus.getIdCard(), 1);
+    CardTypes CarteMonstre ("Monstres");
+    Cards DragonBlancAuxYeuxBleus (0,"Dragon Blanc Aux Yeux Bleus","Monstre de type dragon",CarteMonstre);
+    BOOST_CHECK_EQUAL(DragonBlancAuxYeuxBleus.getIdCard(), 0);
+    DragonBlancAuxYeuxBleus.setIdCard(1);
+    BOOST_CHECK_EQUAL(DragonBlancAuxYeuxBleus.getIdCard(), 1);
 }
 
 BOOST_AUTO_TEST_CASE(TestCalculation)
@@ -29,8 +33,15 @@ BOOST_AUTO_TEST_CASE(TestCalculation)
     BOOST_CHECK_EQUAL(Calcul.getAttackPointsPlayer1(),1500);
 }
 
-BOOST_AUTO_TEST_CASE(TestBoards)
-{
+BOOST_AUTO_TEST_CASE(testSpells) {
+    Spells Sort;
+    Sort.setEffect(Ritual);
+    BOOST_CHECK_EQUAL(Sort.getEffect(),Ritual);
+}
+BOOST_AUTO_TEST_CASE(testTraps) {
+    Traps trap;
+    trap.setEffect(NormalTrap);
+    BOOST_CHECK_EQUAL(trap.getEffect(),NormalTrap);
 
 }
 
@@ -38,7 +49,10 @@ BOOST_AUTO_TEST_CASE(TestMonsters)
 {
 
 }
-
+BOOST_AUTO_TEST_CASE(TestBoards)
+{
+//Boards testBoard ()
+}
 BOOST_AUTO_TEST_CASE(TestSpells)
 {
 
