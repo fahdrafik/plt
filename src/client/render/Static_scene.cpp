b971d45781c415febe9e30c891dd44ea0ba64695
zeicksoule <zeicksoule@gmail.com>
2023-11-22 15:51:31 +0100

    Création Static_scene.cpp

---
+++ src/client/render/Static_scene.cpp
@@ -0,0 +1,15 @@
+#include "Static_scene.h"
+#include "../src/render.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace sf;
+using namespace render;
+using namespace std;
+using namespace state;
+
+Static_scene::Scene(){
+
+}
