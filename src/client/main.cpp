21fcaefe4869e324a537f8039ab3ff7ec422905d
zeicksoule <zeicksoule@gmail.com>
2023-11-27 03:59:30 +0100

    Modification de l'interface pour le render

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -45,12 +45,49 @@ int main(int argc,char* argv[])
     else if (strcmp(argv[1], "render") == 0)
     {
         cout << "Render" << endl;
+
+        /*Création de la scène*/
         Static_scene scene;
-        sf::RenderWindow window(sf::VideoMode(800, 600), "Yu-Gi-Oh!");
+
+        /*Création de la fenètre*/
+
+        sf::RenderWindow window(sf::VideoMode(1000, 800), "Yu-Gi-Oh!");
+
+        /*Première page du jeu*/
+        /*Titre*/
+        sf::Sprite titleSprite;
+        sf::Texture titleTexture;
+        titleTexture.loadFromFile("./Images/yugioh.png");
+        titleSprite.setTexture(titleTexture);
+
+        //sf::Vector2u windowSize = window.getSize();
+        //sf::Vector2u titleTextureSize = titleTexture.getSize();
+        //titleSprite.setPosition((windowSize.x - titleTextureSize.x) / 1.0f, (windowSize.y - titleTextureSize.y) / 1.0f);
+        titleSprite.setScale(0.4f,0.4f);
+        titleSprite.setPosition(300,400);
+
+        /*Boutons pour le menu principal et l'écran titre*/
+        sf::RectangleShape Button_TitleScreen(sf::Vector2f(200, 100));
+        Button_TitleScreen.setPosition(400,550);
+
+        /*Menu du jeu*/
+
+        /*En cours de jeu : vue du premier joueur*/
+
+        /*En cours de jeu : vue du deuxième joueur*/
+
+        /*Menu : Consultation des cartes*/
+
         while (window.isOpen())
         {
             // on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
             sf::Event event;
+
+            window.clear();
+            window.draw(scene.getBackground());
+            window.draw(titleSprite);
+            window.display();
+
             while (window.pollEvent(event))
             {
                 // évènement "fermeture demandée" : on ferme la fenêtre
@@ -61,17 +98,19 @@ int main(int argc,char* argv[])
                 if (event.type == sf::Event::KeyPressed)
                 {
                         if (event.key.code == sf::Keyboard::A) {
-                            std::cout << "A key pressed!" << std::endl;
+                            std::cout << "Menu principal" << std::endl;
                         }
                         else if (event.key.code == sf::Keyboard::B) {
-                            std::cout << "B key pressed!" << std::endl;
+                            std::cout << "Phase de jeu : J1" << std::endl;
                         }
                         else if (event.key.code == sf::Keyboard::C) {
-                            std::cout << "C key pressed!" << std::endl;
+                            std::cout << "Phase de jeu : J2" << std::endl;
+                        }
+                        else if (event.key.code == sf::Keyboard::D) {
+                            std::cout << "Consultation des cartes" << std::endl;
                         }
                 }
             }
-
         }
     }
     return 0;
