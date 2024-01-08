9a3671092fc855eaf6deb78324020a9b4bd271aa
cornic <superrom1.cornic@gmail.com>
2024-01-09 00:14:29 +0100

    engine start cpp and dia

--- src/shared/engine/ModifyHand.cpp
+++ src/shared/engine/ModifyHand.cpp
@@ -2,3 +2,15 @@
 // Created by corte on 1/8/2024.
 //
 #include "ModifyHand.h"
+
+#include <iostream>
+#include <algorithm>
+
+
+using namespace state;
+using namespace std;
+
+namespace engine {
+    ModifyHand::ModifyHand() {}
+
+}
